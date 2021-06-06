7e9989c8f67b72363e94feb746198f7a25a24dcd
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2021-06-06 17:33:28 -0400

    more configs for the prod server

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 const path = require('path');
-const Logger = require('@ptkdev/logger');
-
-const logger = new Logger();
 const server = express();
 server.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
-const host = process.env.HOST || 'khanos-frontend.herokuapp.com';
+const host = process.env.HOST || 'khanos-tictactoe.herokuapp.com';
 server.listen(port, () => {
-  logger.info('The app is running...');
-  logger.info(`http://${host}:${port}`);
+  console.log('The app is running...');
+  console.log(`http://${host}:${port}`);
 });
 module.exports = server;
